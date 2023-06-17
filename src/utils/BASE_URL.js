cb22305b5018822c50bba5cecdc0486acc000910
Ritwik <ritwikkumarrishu@gmail.com>
2023-06-17 22:58:53 +0530

    Modified BASE_URL

--- src/utils/BASE_URL.js
+++ src/utils/BASE_URL.js
@@ -1,2 +1,6 @@
-const BASE_URL = `https://jobify-api-sq50.onrender.com/api/v1`;
+let BASE_URL = `https://jobify-api-sq50.onrender.com/api/v1`;
+
+if (process.env.REACT_APP_NODE_ENV === "development")
+  BASE_URL = "http://localhost:5000/";
+
 export default BASE_URL;
